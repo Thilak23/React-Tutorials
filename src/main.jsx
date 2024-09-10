0cf9626fce1aee1353a4398e930b37103de77b9f
Thilak23 <thilak2k3@gmail.com>
2024-09-10 11:57:35 +0530

    new component added

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import Comp1 from './Components/props/Comp1'
 import PropsDrilling from './Components/Propsdrilling/PropsDrilling'
 import Usestatehook from './Components/Hookks/usestatehook'
+import ToggleDarkMode from './Components/Hookks/ToggleDarkMode'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Comp1/>
     <PropsDrilling/>
     <Usestatehook/>
+    <ToggleDarkMode/>
   </StrictMode>
 )
