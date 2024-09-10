6429122d522bb5cfdac290e84b15ea85c8ddefe2
Thilak23 <thilak2k3@gmail.com>
2024-09-10 11:58:04 +0530

    theme changing

---
+++ src/Components/Hookks/ToggleDarkMode.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import './theme.css'
+
+const ToggleDarkMode = () => {
+
+  const[darkTheme,setDarkTheme] = useState(false);
+
+  const themeChange = () => {
+    setDarkTheme(!darkTheme);
+    if (darkTheme) {
+      document.body.classList.remove('dark');
+    } else {
+      document.body.classList.add('dark');
+    }
+  };
+
+  return (
+    <div>
+      <h1  className='toggle'>ToggleDarkMode</h1>
+      <div className='lak'>
+      <button className='but' onClick={themeChange}>{darkTheme ? 'Light' : 'Dark'}</button>
+      </div>
+    </div>
+  )
+}
+
+export default ToggleDarkMode
