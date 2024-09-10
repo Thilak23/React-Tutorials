b754825cda0243166e3c9f8ebf284dc97313d8e2
Thilak23 <thilak2k3@gmail.com>
2024-09-10 14:20:17 +0530

    added useEffect hookm with example

---
+++ src/Components/Hookks/UseEffectExample.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+
+const UseEffectExample = () => {
+  
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((inc) => inc + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  },[]);
+
+  const reset = () => {
+    setSeconds(0);
+  }
+
+  return (
+    <div style={{marginTop : '50px'}}>
+      <h1>Timer: {seconds}</h1>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+export default UseEffectExample;
