fe58504858c70409b23c986fda97c199b1e00016
Thilak23 <thilak2k3@gmail.com>
2024-09-10 14:19:32 +0530

    imported new component

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import Comp1 from './Components/props/Comp1'
 import PropsDrilling from './Components/Propsdrilling/PropsDrilling'
 import Usestatehook from './Components/Hookks/usestatehook'
 import ToggleDarkMode from './Components/Hookks/ToggleDarkMode'
+import UseEffectExample from './Components/Hookks/UseEffectExample'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -11,5 +12,6 @@ createRoot(document.getElementById('root')).render(
     <PropsDrilling/>
     <Usestatehook/>
     <ToggleDarkMode/>
+    <UseEffectExample/>
   </StrictMode>
 )
